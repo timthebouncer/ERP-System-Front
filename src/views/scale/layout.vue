3ca9681cb3dfec5226c697b99f424bb51837a4a5
Yue <yue.lzy@link8tech.tw>
2021-02-17 17:46:58 +0800

    style(平板->layout): CSS調整

--- src/views/scale/layout.vue
+++ src/views/scale/layout.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="aaa">
-    <div class="title">磅秤機系統</div>
     <router-view />
   </div>
 </template>
 
 <script>
   export default {
-    
+
   }
 </script>
 
 <style lang="scss" scoped>
+  .aaa{
+    display: flex;
+  }
 </style>
