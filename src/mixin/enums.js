0dc4c7e1427e94544246c0336cff988ff2986af8
Yue <yue.lzy@link8tech.tw>
2021-02-20 15:51:55 +0800

    feat(平板->emuns): 單位中文轉換

---
+++ src/mixin/enums.js
@@ -0,0 +1,7 @@
+export const UNIT = [
+    {name: "公斤", value: "KG"},
+    {name: "公克", value: "G"},
+    {name: "件", value: "PIECE"},
+    {name: "台斤", value: "TG"},
+    {name: "包", value:"PACK"}
+]
